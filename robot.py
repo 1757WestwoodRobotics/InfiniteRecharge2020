a8671744f136c67d12fc28fa9d279b0e0174815e
Sean Lendrum <21slendrum@wpsstudents.org>
2020-01-11 14:04:18 -0500

    Add robotmap and drive motors

--- robot.py
+++ robot.py
@@ -1,9 +1,28 @@
 import wpilib
-from wpilib import drive
-from ctre import TalonSRX
+from wpilib import drive, SpeedControllerGroup
+from ctre import WPI_TalonSRX
+from robotmap import *
+from networktables import NetworkTablesInstance
 
 class Robot(wpilib.TimedRobot):
 
-    def robotInit(self):
-        talon1 = TalonSRX(5)
-        talon2 = TalonSRX(9)
+    def robotInita(self):
+        self.balltalon1 = WPI_TalonSRX(motorcontrollerports["ballshooter1"])
+        self.balltalon2 = WPI_TalonSRX(motorcontrollerports["ballshooter2"])
+        self.leftFront = WPI_TalonSRX(motorcontrollerports["leftFront"])
+        self.rightFront = WPI_TalonSRX(motorcontrollerports["rightFront"])
+        self.leftBack = WPI_TalonSRX(motorcontrollerports["leftBack"])
+        self.rightBack = WPI_TalonSRX(motorcontrollerports["rightBack"])
+
+        NetworkTablesInstance.intialize(server="10.17.57.2")
+        self.sd = NetworkTablesInstance.getTable("SmartDashboard")
+    def teleopPeriodic(self):
+        self.leftFrontSpeed = self.sd.getNumber("leftFront", 0)
+        self.rightFrontSpeed = self.sd.getNumber("rightFront", 0)
+        self.leftBackSpeed = self.sd.getNumber("leftBack", 0)
+        self.rightBackSpeed = self.sd.getNumber("rightBack", 0)
+
+        self.leftFront.set(self.leftFrontSpeed)
+        self.rightFront.set(self.rightFrontSpeed)
+        self.leftBack.set(self.leftBackSpeed)
+        self.rightBack.set(self.rightBackSpeed)
---
+++ robotmap.py
@@ -0,0 +1,11 @@
+import wpilib
+from Robot import Robot
+
+motorcontrollerports = {
+    "ballshooter1": 0,
+    "ballshooter2": 1,
+    "leftFront": 5,
+    "rightFront": 6,
+    "leftBack": 7,
+    "rightBack": 8
+}
