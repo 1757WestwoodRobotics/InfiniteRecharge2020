2cfef4b30ed14a5d125af9bde832f57e9e1a05e7
Sean Lendrum <21slendrum@wpsstudents.org>
2020-01-11 16:42:17 -0500

    Co-authored-by: billyGirard <21wgirard@wpsstudents.org>

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 import wpilib
-from wpilib import drive, SpeedControllerGroup
+from wpilib import drive, SpeedControllerGroup, RobotDrive
 from ctre import WPI_TalonSRX
 from robotmap import *
 from networktables import NetworkTables
@@ -7,25 +7,43 @@ from networktables import NetworkTables
 class Robot(wpilib.TimedRobot):
 
     def robotInit(self):
-        # self.balltalon1 = WPI_TalonSRX(motorcontrollerports["ballshooter1"])
-        # self.balltalon2 = WPI_TalonSRX(motorcontrollerports["ballshooter2"])
         self.leftFront = WPI_TalonSRX(motorcontrollerports["leftFront"])
         self.rightFront = WPI_TalonSRX(motorcontrollerports["rightFront"])
         self.leftBack = WPI_TalonSRX(motorcontrollerports["leftBack"])
         self.rightBack = WPI_TalonSRX(motorcontrollerports["rightBack"])
 
+        self.rightDrive = SpeedControllerGroup(self.rightFront, self.rightBack)
+        self.leftDrive = SpeedControllerGroup(self.leftFront, self.rightFront)
+
+        self.differentialDrive = drive.DifferentialDrive(self.leftDrive, self.rightDrive)
+
+        self.balltalon1 = WPI_TalonSRX(motorcontrollerports["ballshooter1"])
+        self.balltalon2 = WPI_TalonSRX(motorcontrollerports["ballshooter2"])
+
         NetworkTables.initialize(server="10.17.57.2")
         self.sd = NetworkTables.getTable("SmartDashboard")
+        self.sd.putBoolean("Run Motor Forward", False)
+        self.sd.putBoolean("Run Motor Backward", False)
+    
     def teleopPeriodic(self):
-        self.leftFrontSpeed = self.sd.getNumber("leftFront", 0)
-        self.rightFrontSpeed = self.sd.getNumber("rightFront", 0)
-        self.leftBackSpeed = self.sd.getNumber("leftBack", 0)
-        self.rightBackSpeed = self.sd.getNumber("rightBack", 0)
+        self.runMotorForward = self.sd.getBoolean("Run Motor Forward", False)
+        self.runMotorBackward = self.sd.getBoolean("Run Motor Backward", False)
 
-        self.leftFront.set(self.leftFrontSpeed)
-        self.rightFront.set(self.rightFrontSpeed)
-        self.leftBack.set(self.leftBackSpeed)
-        self.rightBack.set(self.rightBackSpeed)
+        if self.runMotorForward:
+            self.leftFront.set(1)
+            self.rightFront.set(1)
+            self.leftBack.set(-1)
+            self.rightBack.set(-1)
+        elif self.runMotorBackward:
+            self.leftFront.set(-1)
+            self.rightFront.set(-1)
+            self.leftBack.set(1)
+            self.rightBack.set(1)
+        else:
+            self.leftFront.set(0)
+            self.rightFront.set(0)
+            self.leftBack.set(0)
+            self.rightBack.set(0)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
