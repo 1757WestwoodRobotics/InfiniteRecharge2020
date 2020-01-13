6361c47ff9a0477bb4bc944fe261e852820bf587
Sean Lendrum <21slendrum@wpsstudents.org>
2020-01-13 09:48:29 -0500

    Drive Code

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 import wpilib
-from wpilib import drive, SpeedControllerGroup, RobotDrive
+from wpilib import drive, SpeedControllerGroup, RobotDrive, XboxController, interfaces
+from wpilib.interfaces import GenericHID
 from ctre import WPI_TalonSRX
 from robotmap import *
 from networktables import NetworkTables
@@ -17,33 +18,19 @@ class Robot(wpilib.TimedRobot):
 
         self.differentialDrive = drive.DifferentialDrive(self.leftDrive, self.rightDrive)
 
-        self.balltalon1 = WPI_TalonSRX(motorcontrollerports["ballshooter1"])
-        self.balltalon2 = WPI_TalonSRX(motorcontrollerports["ballshooter2"])
+        self.ballTalon1 = WPI_TalonSRX(motorcontrollerports["ballshooter1"])
+        self.ballTalon2 = WPI_TalonSRX(motorcontrollerports["ballshooter2"])
 
         NetworkTables.initialize(server="10.17.57.2")
         self.sd = NetworkTables.getTable("SmartDashboard")
-        self.sd.putBoolean("Run Motor Forward", False)
-        self.sd.putBoolean("Run Motor Backward", False)
-    
-    def teleopPeriodic(self):
-        self.runMotorForward = self.sd.getBoolean("Run Motor Forward", False)
-        self.runMotorBackward = self.sd.getBoolean("Run Motor Backward", False)
 
-        if self.runMotorForward:
-            self.leftFront.set(1)
-            self.rightFront.set(1)
-            self.leftBack.set(-1)
-            self.rightBack.set(-1)
-        elif self.runMotorBackward:
-            self.leftFront.set(-1)
-            self.rightFront.set(-1)
-            self.leftBack.set(1)
-            self.rightBack.set(1)
-        else:
-            self.leftFront.set(0)
-            self.rightFront.set(0)
-            self.leftBack.set(0)
-            self.rightBack.set(0)
+        self.xboxController = XboxController(0)
+
+    def teleopPeriodic(self):
+        self.speed = self.xboxController.getY(GenericHID.Hand.kLeft)
+        self.rotation = self.xboxController.getX(GenericHID.Hand.kRight)
+        
+        self.differentialDrive.arcadeDrive(self.speed, self.rotation, True)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
