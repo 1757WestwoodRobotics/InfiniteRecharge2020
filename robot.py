c80348ac2ca7c1b2e325c3fc194b63d0b8b140ce
Christopher Aloisio <caloisio@energid.com>
2020-02-02 16:25:21 -0500

    ENHANCEMENT: Updated requirements and robot.py for rev color testing

--- robot.py
+++ robot.py
@@ -1,7 +1,23 @@
 import wpilib
+from rev.color import ColorSensorV3
+
 
 class Robot(wpilib.TimedRobot):
-    pass
+    def robotInit(self):
+        self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
 
+    def robotPeriodic(self):
+        # Get the sensor attributes
+        color = self.colorSensor.getColor()
+        ir = self.colorSensor.getIR()
+
+        # Get the individual components of the color
+        red = color.red
+        blue = color.blue
+        green = color.green
+
+        # Get the approximate proximity of an object
+        proximity = self.colorSensor.getProximity()
+        
 if __name__ == "__main__":
     wpilib.run(Robot)
