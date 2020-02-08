3b7b7ea3e11992126d414f813ed29afbfdc6d2ae
Robotics <westwoodrobotics@gmail.com>
2020-02-08 11:25:19 -0500

    BUGFIX: Remove unecessary square bracket

--- subsystems/ballshooter.py
+++ subsystems/ballshooter.py
@@ -9,4 +9,4 @@ class Ballshooter(Subsystem):
         Subsystem.__init__(name=Ballshooter)
         
         self.ballTalon1 = WPI_TalonSRX(Can.BallShooter1)
-        self.ballTalon2 = WPI_TalonSRX(Can.BallShooter2])
+        self.ballTalon2 = WPI_TalonSRX(Can.BallShooter2)
