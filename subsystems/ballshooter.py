c2bd58b413775a1e4033757511283fed93826a0c
Christopher Aloisio <caloisio@energid.com>
2020-02-08 11:17:38 -0500

    BUGFIX: Call ballshooter super class in init directly (see https://robotpy.readthedocs.io/en/stable/2020_notes.html#notes)

--- subsystems/ballshooter.py
+++ subsystems/ballshooter.py
@@ -6,7 +6,7 @@ from wpilib.command.subsystem import Subsystem
 class Ballshooter(Subsystem):
 
     def __init__(self):
-        super().__init__(name=Ballshooter)
+        Subsystem.__init__(name=Ballshooter)
         
         self.ballTalon1 = WPI_TalonSRX(Can.BallShooter1)
         self.ballTalon2 = WPI_TalonSRX(Can.BallShooter2])
