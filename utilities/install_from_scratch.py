87e7ff86762808ef7fdcb272062362825f40fc0d
Christopher Aloisio <caloisio@energid.com>
2020-02-08 10:40:42 -0500

    ENHANCEMENT: Added git repo update script and made the utility scripts executable from shell directly (do not need to add "python" in the front)

--- utilities/install_from_scratch.py
+++ utilities/install_from_scratch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from subprocess import call
 
 call("pip install -r requirements.txt", shell=True)
--- utilities/upgrade.py
+++ utilities/upgrade.py
@@ -2,4 +2,5 @@ import pkg_resources
 from subprocess import call
 
 packages = [dist.project_name for dist in pkg_resources.working_set]
+print("packages:\n{}".format(packages))
 call("pip install --upgrade " + ' '.join(packages), shell=True)
