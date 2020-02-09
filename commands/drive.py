02569f4a79e1f4ce5807a286094d5f9b46b3caf6
Robotics <westwoodrobotics@gmail.com>
2020-02-09 12:20:54 -0500

    ENHANCEMENT: Add drive functionality from xbox controller

--- commands/drive.py
+++ commands/drive.py
@@ -1,15 +1,20 @@
 import wpilib
 from wpilib.command import Command
-from subsystems.drivetrain import Drivetrain
+from wpilib.interfaces import GenericHID
+import subsystems
 
 class Drive(Command):
 
     def __init__(self):
         Command.__init__(name=Drive, subsystem=Drivetrain)
 
-        self.requires(self.getRobot().drivetrain)
+        self.requires(subsystems.team1757Subsystems.drivetrain)
 
     def execute(self):
-        self.speed = self.getRobot().oi.xboxController.getY()
+        self.speed = -self.getRobot().oi.xboxController.getRawAxis(1)
+        self.rotation = self.getRobot().oi.xboxController.getRawAxis(4)
+        
+        subsystems.team1757Subsystems.drivetrain.arcadeDrive(self.speed, self.rotation)
 
-        self.getRobot().drivetrain.arcadeDrive()
+    def isFinished(self):
+        return False
