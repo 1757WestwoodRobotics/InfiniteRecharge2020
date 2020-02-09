a69a880844dc84559c1c42fd68842872b6858ab3
Christopher Aloisio <chris.aloisio@gmail.com>
2020-02-09 10:33:02 -0500

    BUGFIX: Modified repo script to use ff-only pulls instead of rebase

--- utilities/repo_update.py
+++ utilities/repo_update.py
@@ -4,7 +4,7 @@ from subprocess import call
 
 git_fetch_command = "git fetch"
 git_stash_save_command = "git stash save -u \"repo_update_temp\""
-git_pull_rebase_command = "git pull --rebase"
+git_pull_rebase_command = "git pull --ff-only"
 git_stash_pop_command = "git stash pop"
 call(git_fetch_command 
      + " && "
