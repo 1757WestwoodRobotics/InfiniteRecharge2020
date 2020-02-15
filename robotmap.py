6d7faf3028f7c6901b5582edb01ee1ee4ab5740f
Rob Stairs <rstairs@comcast.net>
2020-02-15 15:15:21 -0500

    Break up string defining "Can" constants tuple for readability

--- robotmap.py
+++ robotmap.py
@@ -1,21 +1,24 @@
 import collections
 
-#can = {
-#    "leftFront": 0,
-#    "rightFront": 1,
-#    "leftBack": 2,
-#    "rightBack": 3,
-#    "ballshooter1": 4,
-#    "ballshooter2": 5,
-#    "controlPanel": 6
-#}
 
 # This is better way to set up constants, similar to an enum in Java or C++
 # Tuples are immutable, so you don't have to worry about someone accidentally overwriting their values.
 # They are truly constant.  Also, the syntax for using the values is cleaner than for a dictionary, e.g.
 # value = Can.LeftFront
 # Code style point: Capitalizing the first letter also makes them stand out in your code as constants.
-Can = collections.namedtuple("_", "leftFront rightFront leftBack rightBack collector ballholder1 ballholder2 ballShooterLower ballShooterUpper turret controlPanel lift1 lift2") (*range(13))
+Can = collections.namedtuple("_", ("leftFront "
+                                   "rightFront "
+                                   "leftBack "
+                                   "rightBack "
+                                   "collector "
+                                   "ballholder1 "
+                                   "ballholder2 "
+                                   "ballShooterLower "
+                                   "ballShooterUpper "
+                                   "turret "
+                                   "controlPanel "
+                                   "lift1 "
+                                   "lift2")) (*range(13))
 
 
 colors = {
@@ -25,8 +28,4 @@ colors = {
     "yellow": 3
 }
 
-#neutralModes = {
-#    "coast": 1,
-#    "brake": 2
-#}
 NeutralModes = collections.namedtuple("_", "Coast Brake") (*range(2))
