2ed6e89a967df4886d66c296314f2a17825d65a2
Robert <rstairs@comcast.net>
2020-02-19 17:56:06 -0500

    Fix spelling error in comment

--- robotmap.py
+++ robotmap.py
@@ -4,7 +4,7 @@ import collections
 # This is better way to set up constants, similar to an enum in Java or C++
 # Tuples are immutable, so you don't have to worry about someone accidentally overwriting their values.
 # They are truly constant.  Also, the syntax for using the values is cleaner than for a dictionary, e.g.
-# value = Can.LeftFront
+# value = Can.leftFront
 # Code style point: Capitalizing the first letter also makes them stand out in your code as constants.
 Can = collections.namedtuple("_", ("leftFront "
                                    "rightFront "
