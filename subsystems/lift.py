35f7dc823a0075cf483a63ef583b09ddc96f7641
Robotics <westwoodrobotics@gmail.com>
2020-02-21 15:56:37 -0500

    in progress

--- subsystems/lift.py
+++ subsystems/lift.py
@@ -9,36 +9,36 @@ class Lift(Subsystem):
         Subsystem.__init__(self, "Lift")
 
         self.lift1 = WPI_TalonSRX(Can.lift1)
-        # lift1=motor
-        self.reverseLimitSwitch = wpilib.DigitalInput(1)
-        # 20 arbitrary value
+        # self.reverseLimitSwitch = wpilib.DigitalInput(1)
         self.upperlimitvalue= 21
         self.lowerlimitvalue= 0
-        # configReverseLimitSwitchSource()
+
         self.x = self.lift1.isFwdLimitSwitchClosed()
         self.y = self.lift1.isRevLimitSwitchClosed()
         #isFwdLimitSwitchClosed get the status of limit switch (returns an int??)
-    def periodic(self):
-        Subsystem.periodic(self, "Lift")
-        
-        
-        print(self.x)
-        print(self.y)
-   
-
+    
     def setSpeed(self, speed):
         self.lift1.set(speed)
 
-#used to read the upper limit switch value from the motor
-    def isAtUpperLimit(self, value, output):
-        if value >= self.upperlimitvalue:
-            if self.reverseLimitSwitch.get():
-                output = max(0, output)
-                return self.lift1.set(output)
+    def periodic(self):
+        Subsystem.periodic(self, "Lift")
+    
+        '''get value, if it exceeds upper limit value, print limit switch value'''
+        #used to read the upper limit switch value from the motor
+        if self.x >= self.upperlimitvalue:
+            print(self.x)
+    
+        if self.y <= self.lowerlimitvalue:
+            print(self.y)
+    
+    # def isAtUpperLimit(self, value, output):
+    #     if value >= self.upperlimitvalue:
+    #         if self.reverseLimitSwitch.get():
+    #             output = max(0, output)
+    #             self.lift1.set(output)
 
-#used to read the lower limit switch value from the motor
-    def isAtLowerLimit(self, value, output):
-        if value <= self.lowerlimitvalue:
-            if self.reverseLimitSwitch.get():
-                output = max(0, output)
-                return self.lift1.set(output)
+    # def isAtLowerLimit(self, value, output):
+    #     if value <= self.lowerlimitvalue:
+    #         if self.reverseLimitSwitch.get():
+    #             output = max(0, output)
+    #             self.lift1.set(output)
