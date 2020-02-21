011e6104798f0ca6e8ae08f375679b5c9f12bf62
Sean Lendrum <sean@whsrobotics.org>
2020-02-21 15:54:41 -0500

    Add documentation

--- commands/driveforward.py
+++ commands/driveforward.py
@@ -5,11 +5,21 @@ from wpilib.interfaces import GenericHID
 import subsystems
 
 class DriveForward(TimedCommand):
+    '''
+    Command to drive robot forward without user control
 
-    def __init__(self, output = 1, timeout = 0):
+    Parameters:
+
+    Output: Motor output from -1 to 1; if not specified, default is 1
+
+    Timeout: Time in seconds that the command runs for ; if not specified, deafault is 2
+    '''
+
+    def __init__(self, output = 1, timeout = 2):
         TimedCommand.__init__(self, "Drive Forward", timeout)
         self.requires(subsystems.team1757Subsystems.drivetrain)
 
+        self.timeout = timeout
         self.output = output
 
     def execute(self):
