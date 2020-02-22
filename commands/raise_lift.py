dd7944f4418e2985adba1972b196d7f8558b539f
Robotics <westwoodrobotics@gmail.com>
2020-02-22 14:52:01 -0500

    in progress

--- commands/raise_lift.py
+++ commands/raise_lift.py
@@ -1,4 +1,3 @@
-#  Uses the Lifter subsystem to raise the lifter until the upper limit is reached (and keep it there)
 import wpilib
 from wpilib.command import Command
 import subsystems
@@ -11,21 +10,42 @@ class RaiseLift(Command):
         Command.__init__(self, "RaiseLift")
 
         self.requires(subsystems.team1757Subsystems.lift)
-        self.forwardLimitSwitch = wpilib.DigitalInput(1)
         
         self.lift1 = WPI_TalonSRX(Can.lift1)
         self.speed = speed
 
     def execute(self):
-        # if button is pressed (in oi.py), motors will run when upper limit value is not reached, if it is, motors will stop
-        if subsystems.team1757Subsystems.lift.fwdstatus < subsystems.team1757Subsystems.lift.upperlimitvalue:
+        
+        if subsystems.team1757Subsystems.lift.fwdstatus:
             subsystems.team1757Subsystems.lift.setSpeed(self.speed)
         else:    
             self.speed = 0
             subsystems.team1757Subsystems.lift.setSpeed(self.speed)
-    
+
     def end(self):
         subsystems.team1757Subsystems.lift.setSpeed(0)
 
     def isFinished(self):
         return False
+
+'''
+configure motor controller to turn on limit switch checking
+
+if limit/target position not yet reached, motors run at a set speed
+
+encoder counts is read as a position, multiply factor by position in inches
+'''
+def robotInit(self):
+        self.forwardLimitSwitch = wpilib.DigitalInput(1)
+        self.reverseLimitSwitch = wpilib.DigitalInput(2)
+        self.joystick1 = wpilib.Joystick(1)
+        self.motor = wpilib.Talon(1)
+
+    def teleopPeriodic(self):
+        output = self.Joystick1.getY()
+        if self.forwardLimitSwitch.get():
+            output = min(0, output)
+        elif self.reverseLimitSwitch.get():
+            output = max(0, output)
+# .get = isllimitswtichclosed aka fwdstatus?
+        motor.set(output)
--- subsystems/lift.py
+++ subsystems/lift.py
@@ -1,43 +1,44 @@
-# get value, if it exceeds upper limit value, print limit switch value
 import wpilib
 from ctre import WPI_TalonSRX
+from ctre import ControlMode
 from robotmap import Can
 from wpilib.command import Subsystem
 from math import pi
 
 class Lift(Subsystem):
- 
-    sprocketdiameter =3.341
-    gearratio= 14.2
-    inch = 21
+
+    sprocketdiameter = 3.341
+    gearratio = 14.2
+    inch = 1
     encodercounts = 2048
 
     sprocketcirc = sprocketdiameter * pi
-    sprocketrot = sprocketcirc / inch 
-    motorrot = gearratio / sprocketrot
-    factor = encodercounts / motorrot
-
-    print(factor)
+    sprocketrot = inch / sprocketcirc 
+    motorrot = gearratio * sprocketrot
+    inches_to_encodercounts_factor = encodercounts * motorrot
 
     def __init__(self):
         Subsystem.__init__(self, "Lift")
         # lift1 = motor 11
         self.lift1 = WPI_TalonSRX(Can.lift1)
-        self.upperlimitvalue= 21
-        self.lowerlimitvalue= 0
-
-        self.fwdstatus = self.lift1.isFwdLimitSwitchClosed()
-        self.revstatus = self.lift1.isRevLimitSwitchClosed()
-        #isFwdLimitSwitchClosed get the status of limit switch (returns an int)
+        self.upperlimitvalue = inches_to_encodercounts_factor
+        self.lowerlimitvalue = 0
 
     def setSpeed(self, speed):
         self.lift1.set(speed)
 
-    def periodic(self):
-        #used to read the upper limit switch value from the motor
+    def setPosition(self, position_in_inches):
+        position_in_encoder_counts = position_in_inches * inches_to_encodercounts_factor
+        self.lift1.set(ControlMode.Position, position_in_encoder_counts)
         
-        if self.fwdstatus >= self.upperlimitvalue:
+    def periodic(self):
+        #used to read the limit switch value from the motor
+
+        self.fwdstatus = self.lift1.isFwdLimitSwitchClosed()
+        self.revstatus = self.lift1.isRevLimitSwitchClosed()
+        #get the status of limit switch (returns an int)
+
+        if fwdstatus:
             print(self.fwdstatus)
-    
-        if self.revstatus <= self.lowerlimitvalue:
+        if revstatus:
             print(self.revstatus)
