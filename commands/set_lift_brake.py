393d697fada3fbd3afdcc92c5c407d1dfe6654c9
Robotics <westwoodrobotics@gmail.com>
2020-03-04 22:24:06 -0500

    Slightly change and fix documentation for parameters

--- commands/set_lift_brake.py
+++ commands/set_lift_brake.py
@@ -3,10 +3,17 @@ import subsystems
 from wpilib.command import Command
 
 class SetLiftBrake(Command):
+    '''
+    Command to set the brake for the elevator
 
-    def __init__(self, forward: bool = True):
+    Parameters:
+
+    Engaged: If true, the brake will engage, if false, it will retract
+    '''
+    def __init__(self, engaged):
         Command.__init__(self, "Set Lift Brake")
-        self.direction = 1 if forward else -1
+        
+        self.direction = 1 if engaged else -1
 
     def execute(self):
         subsystems.team1757Subsystems.lift.setLiftBrake(self.direction)
